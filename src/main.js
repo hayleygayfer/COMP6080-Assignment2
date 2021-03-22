bf4f717062b47d5379e69045afac919787818be7
z5312096 <hayley@gayfer.id.au>
2021-03-22 12:36:40 +1100

    changes

--- src/main.js
+++ src/main.js
@@ -4,11 +4,39 @@ import { fileToDataUrl } from './helpers.js';
 
 // This url may need to change depending on what port your backend is running
 // on.
-const api = new API('http://localhost:5000');
+//const api = new API('http://localhost:5000');
 
 // Example usage of makeAPIRequest method.
-api.makeAPIRequest('dummy/user')
-    .then(r => console.log(r));
+/*api.makeAPIRequest('dummy/user')
+    .then(r => console.log(r));*/
+
+let user_token = '';
+
+document.getElementById("submit_login").addEventListener('click', () => {
+    const login_body = {
+        "username": document.getElementById('username').value,
+        "password": document.getElementById('password').value,
+    };
+    const result = fetch('http://localhost:5000/auth/login', {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(login_body),
+    }).then((data) => {
+        if (data.status === 403) {
+            alert('Incorrect login details!');
+        } else if (data.status === 200) {
+            data.json().then(result => {
+                user_token = result.token;
+                alert(result.token);
+            })
+        }
+    }).catch((error) => {
+        alert('Error: ', error);
+    });
+});
 
 
 
